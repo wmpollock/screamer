fb1711556334c0c4edb2b5c4ed51fce392bf12bb
William Pollock [redbox] <wmpollock@gmail.com>
2019-03-17 01:22:17 -0700

    Last gasp before pushing off to remote, though now I clobbered things :/Z

--- index.php
+++ index.php
@@ -25,25 +25,53 @@ $if_i_dont_assign_it_spews_before_content = new Redbox(array(
   </div>
   <form>
 <?php
-
+$start_outer_container = "<div class='row actions'>\n";
+$start_outer_grid = "\t<div class='col-md-6 col-12'>\n";
+$start_inner_grid = "\t\t<div class='row justify-content-between'>\n";
 foreach (SRC_DIRS as $src_dir) {
     // $dir_fh = opendir($src_dir);
-    echo "<h1>", preg_replace("/_/", " ", $src_dir), "</h1>";
-    echo "<div class='d-flex align-content-center flex-wrap actions'>";
+    echo "\n<h1>", preg_replace("/_/", " ", $src_dir), "</h1>\n";
+    echo $start_outer_container,
+         $start_outer_grid,
+         $start_inner_grid;
+    $counter = 0;
     foreach (preg_grep('/\.(mp3|wav)$/', scandir("./audio/" . $src_dir)) as $file) {
-
+        if ($counter && ($counter % 4 == 0 )) {
+            echo "\t\t</div>\n\t</div>\n";
+            if ($counter % 8 == 0 ) {
+                echo "</div>\n",
+                $start_outer_container;
+            }
+            echo $start_outer_grid,
+                 $start_inner_grid;
+        }
+        ++$counter;
         $path = "$src_dir/$file";
         $icon_file = "avatars/$src_dir-" . preg_replace('/\..+?$/', '', $file) . ".png";
         if (! file_exists($icon_file))
         {
             $url = "https://api.adorable.io/avatars/80/$src_dir-$file.png";
-            //print ("<p>Getting $icon_file from $url</p>\n");
+            print ("<!--Getting $icon_file from $url -->\n");
             file_put_contents($icon_file, fopen($url, 'r'));
         }
-        echo "<div class='p-0 mr-auto'><a href='#' data-src='$path'><img src='$icon_file'/></a></div>\n";
+        echo "\t\t\t<div class='col-3'>\n",
+             "\t\t\t\t<a href='#' data-src='$path'><img src='$icon_file' class='img-fluid'/></a>\n",
+             "\t\t\t</div>\n";
 
     }
-    echo "</div>";
+    
+    // When we close it, its nice to consider packing the remaining elements in the
+    // grid so they don't space all "we don't care."
+    $gridno = $counter % 4;
+    if ($counter > 0) {
+        $infill = 4 - $gridno;
+        if ($infill) {
+            $grid_size = $infill * 3; // 8/2 = 4 cells of 3 units in the subgrids
+            echo "\t\t\t<div class='col-$grid_size'>&nbsp;</div>\n";
+        }
+    }
+    
+    echo "\t\t</div>\n\t</div>\n</div>";
 }
 ?>
   </form>
--- screamer.js
+++ screamer.js
@@ -1,6 +1,24 @@
+// SCREAMER
+
 $("document").ready(function() {
     $(".actions a").click(function(e) {
+    	var $clickyThing = $(this);
+    	
 	e.preventDefault();
-	$.get("play.php?audio=" + $(this).data('src'));
+	
+	$.get("play.php?audio=" + $clickyThing.data('src'),
+		  function() {
+		// Done playing.thanks
+		if ($clickyThing.data('instances') == 1) {
+			$clickyThing.removeClass('playing');
+		}
+		$clickyThing.data('instances', $clickyThing.data('instances') - 1);
+	});
+	// Tag this file as playing and count how many instances we've got.
+	// I mean, sure, we should allow + 1 buttonpresses, yes?  YESSS.
+	if (!$clickyThing.data('instances')) {
+		$clickyThing.data('instances', 0);
+	}
+	$clickyThing.addClass('playing').data('instances', $(this).data('instances') + 1);
     });
 });
