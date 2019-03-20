00f9b31a6d23813ccddd25ffc060b1a5ede49a20
William Pollock <wmpollock@users.noreply.github.com>
2019-03-19 22:36:02 -0700

    fiddle fiddle

--- play.php
+++ play.php
@@ -1,13 +1,12 @@
 <pre>
 <?php
 
-
 if ($_GET) {
    $audio = escapeshellcmd("./audio/" . $_GET['audio']);
 } else {
-  $audio = "./audio/evil_laughs/wickedwitchlaugh.mp3";
+   $audio = "./audio/evil_laughs/wickedwitchlaugh.mp3";
 }
 
-//system("/usr/bin/mplayer $audio > lastrun.log");
 system("/usr/bin/mplayer -ao pulse $audio 2> lastrun.err");
+
 ?>
